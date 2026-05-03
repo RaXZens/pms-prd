bdc43079b313035275285f861e7ec2bf389b5305
RaXZens <leekung119@gmail.com>
2026-05-04 03:06:39 +0700

    fix deploy railway

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,3 +1,8 @@
+import { webcrypto } from 'crypto';
+if (!globalThis.crypto) {
+  (globalThis as any).crypto = webcrypto;
+}
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
